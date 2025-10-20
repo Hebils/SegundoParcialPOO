bcf262137b47d8dc37640f93380a8f8367575ce9
AngelloCortesPalomares <cortes.palomares.angello00@gmail.com>
2025-10-20 18:37:10 -0500

    creacion del proyecto

---
+++ src/main/java/com/mycompany/segundoparcialpoo/SegundoParcialPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.segundoparcialpoo;
+
+/**
+ *
+ * @author Angello
+ */
+public class SegundoParcialPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
