f7c9ee69d28755bead7d505355a667ccbb6185ea
AngelloCortesPalomares <cortes.palomares.angello00@gmail.com>
2025-10-20 20:41:39 -0500

    Creacion de las clases Profesor y Estudiante, y puse un toString en Persona para poder leer los datos que se recolectan

---
+++ src/main/java/Modelo/Profesor.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Angello
+ */
+public class Profesor {
+    
+    private int cedula;
+    private String materia;
+    private int horas;
+    private double salario;
+    private double prestaciones;
+    
+}
