770df03d0b67bfd3fa5cf645ad729a17df494aa3
AngelloCortesPalomares <cortes.palomares.angello00@gmail.com>
2025-10-20 20:44:22 -0500

    Funciones de calcular salario mensual y prestaciones sociales en la clase Profesor

--- src/main/java/Modelo/Profesor.java
+++ src/main/java/Modelo/Profesor.java
@@ -15,5 +15,54 @@ public class Profesor {
     private int horas;
     private double salario;
     private double prestaciones;
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getPrestaciones() {
+        return prestaciones;
+    }
+
+    public void setPrestaciones(double prestaciones) {
+        this.prestaciones = prestaciones;
+    }
     
+    public double calcularPagoMensual() {
+        double pagoBase = salario * horas;
+        double bonoPreparacion = pagoBase * 0.20;
+        return pagoBase + bonoPreparacion;
+    }
+    
+    public double calcularPrestacionesSociales() {
+        return calcularPagoMensual() * 0.17;
+    }
 }
